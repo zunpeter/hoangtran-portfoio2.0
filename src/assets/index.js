64d4eba77bd2791fec17b74db5ceade7532733c5
Hoang Tran <66261053+zunpeter@users.noreply.github.com>
2023-06-01 00:04:44 +0700

    import assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import backend from "./backend.png";
+import flutter from "./flutter.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
@@ -32,7 +32,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
-  backend,
+  flutter,
   creator,
   mobile,
   web,
