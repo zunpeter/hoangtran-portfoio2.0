7939d258b23b0c34be7c23186de4c768901773d3
Hoang Tran <66261053+zunpeter@users.noreply.github.com>
2023-06-01 22:05:02 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
-import Sound from 'react-sound';
+// import Sound from 'react-sound';
 import { About, Contact, Education, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Sound url="src/assets/sinnesloschen-beam.mp3"
+    {/* <Sound url="src/assets/sinnesloschen-beam.mp3" */}
     />
       <div className='relative z-0 bg-primary'>
         {/* <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'> */}
